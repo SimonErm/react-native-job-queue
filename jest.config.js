238eabf1b248659da896493f8248599993c404f6
Simon Ermler <simon_ermler@web.de>
2019-08-14 01:08:51 +0200

    add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'react-native',
+    transform: {
+        '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js'
+    },
+    testPathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/node_modules/', '<rootDir>/example/'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+};
