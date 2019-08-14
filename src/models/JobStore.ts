621d4fef3cab9a4cb6d74afe4f6b91e80ed4521b
Simon Ermler <simon_ermler@web.de>
2019-08-15 01:26:17 +0200

    update function names in interface

--- src/models/JobStore.ts
+++ src/models/JobStore.ts
@@ -3,9 +3,9 @@ import { Job } from './Job';
 export interface JobStore {
     addJob(job: Job): Promise<void>;
     getJobs(): Promise<Job[]>;
-    getJobsToExecute(): Promise<Job[]>;
+    getNextJob(): Promise<Job>;
     getJobsForWorker(name: string, count: number): Promise<Job[]>;
     updateJob(job: Job): Promise<void>;
-    deleteJob(job: Job): Promise<void>;
+    removeJob(job: Job): Promise<void>;
     deleteAllJobs(): Promise<void>;
 }
