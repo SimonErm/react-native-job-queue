4c3cef7c6d4da380d632c99e2d435aecfe7fecf1
Simon Ermler <s.ermler@evosec.de>
2019-08-15 15:37:08 +0200

    add Method Map for rn

--- ios/JobQueue.m
+++ ios/JobQueue.m
@@ -2,6 +2,9 @@
 
 @interface RCT_EXTERN_MODULE(JobQueue, NSObject)
 
-RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(nonnull NSNumber *)date)
+RCT_EXTERN_METHOD(addJob:(NSDictionary *)job)
+RCT_EXTERN_METHOD(removeJob:(NSDictionary *)job)
+RCT_EXTERN_METHOD(getNextJob:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(getJobsForWorker:(NSString *)name count:(NSInteger) count resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 
 @end
