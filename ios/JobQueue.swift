4957fccb59dcc5bc47b714afd85cf2b93bf78557
Simon Ermler <s.ermler@evosec.de>
2019-08-15 15:43:52 +0200

    implement ios module functions

--- ios/JobQueue.swift
+++ ios/JobQueue.swift
@@ -23,6 +23,74 @@ public class JobQueue:NSObject{
         }
         
     }
+    
+    @objc
+    public func addJob(_ job:[String:Any]){
+        if(db != nil){
+            do{
+                try db?.add(job: Job.createJobFromDictionary(job: job))
+            }catch{
+                print("Couln't add Job to Database: ",error)
+            }
+        }
+    }
+    
+    @objc
+    public func removeJob(_ job:[String:Any]){
+        if(db != nil){
+            do{
+                try db?.delete(job: Job.createJobFromDictionary(job: job))
+            }catch{
+                print("Couln't remove job Job to Database: ",error)
+            }
+        }
+    }
+    @objc
+    public func updateJob(_ job:[String:Any]){
+        if(db != nil){
+            do{
+                try db?.update(job: Job.createJobFromDictionary(job: job))
+            }catch{
+                print("Couln't update job Job to Database: ",error)
+            }
+        }
+    }
+    
+    @objc
+    public func getNextJob(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock){
+        if(db != nil){
+            if let job =  db?.getNextJob(){
+                resolve(job.toDictionary())
+            }else{
+                resolve([String:Any]())
+            }
+            
+        }
+    }
+    
+    @objc
+    public func getJobsForWorker(_ name:String, count:Int, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock){
+        if(db != nil){
+            if let jobs =  db?.getJobsForWorker(name: name as NSString, count: Int32(count)){
+                var jobsAsDictionaryArray=[[String:Any]]()
+                for job in jobs{
+                    var job=job
+                    job.active = 1
+                    do{
+                        try db?.update(job: job)
+                        jobsAsDictionaryArray.append(job.toDictionary())
+                    }catch{
+                        print("Couln't update job Job to Database: ",error)
+                    }
+                    
+                }
+                resolve(jobsAsDictionaryArray)
+            }else{
+                resolve([[String:Any]]())
+            }
+            
+        }
+    }
     @objc static func requiresMainQueueSetup() -> Bool {
         return false
     }
