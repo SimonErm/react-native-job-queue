01f62330e5d9bd116fb426459de09931c9cdd7cd
Simon Ermler <simon_ermler@web.de>
2019-08-18 22:27:27 +0200

    update docusaurus config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,14 @@ const users = [
 const siteConfig = {
     title: 'React Native Job Queue', // Title for your website.
     tagline: 'React Native Job Queue is an easy to use queuing library',
-    url: 'https://your-docusaurus-test-site.com', // Your website URL
-    baseUrl: '/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
 
-    // Used for publishing and more
-    projectName: 'React Native Job Queue',
-    organizationName: 'facebook',
+    url: 'https://simonerm.github.io', // Your website URL
+    baseUrl: '/react-native-job-queue/',
+    organizationName: 'SimonErm',
+    projectName: 'react-native-job-queue',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
