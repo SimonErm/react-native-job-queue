af5d46917fd1d181940d9334d5c30545c88be7d2
Simon Ermler <simon_ermler@web.de>
2020-01-28 23:55:25 +0100

    fix tests

--- src/__tests__/Queue.test.ts
+++ src/__tests__/Queue.test.ts
@@ -1,19 +1,28 @@
 import queue from '../Queue';
 import { Worker } from '../Worker';
 
+export interface Payload {
+    test: string;
+}
 describe('Queue Basics', () => {
+    beforeEach(() => {
+        queue.removeWorker('testWorker');
+    });
     it('add Workers', () => {
-        queue.addWorker(new Worker('testWorker', async (payload: any) => {}));
+        queue.addWorker(
+            new Worker<Payload>('testWorker', async (payload: Payload) => {})
+        );
         const workers = queue.registeredWorkers;
         const workerNames = Object.keys(workers);
         expect(workerNames.length).toEqual(1);
         expect(workerNames[0]).toEqual('testWorker');
     });
     it('run queue', (done) => {
-        const executer = async (payload: any) => {
+        const executer = async (payload: Payload) => {
             done();
         };
-        queue.addWorker(new Worker('testWorker', executer));
+        queue.addWorker(new Worker<Payload>('testWorker', executer));
+        queue.addJob('testWorker', {});
         queue.start();
     });
 });
