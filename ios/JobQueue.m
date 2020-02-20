6748bfa65646ecd758758d74f092e5b9e69944e0
sermler <s.ermler@evosec.de>
2020-02-20 09:29:08 +0100

    add missing objc->swift mapping

--- ios/JobQueue.m
+++ ios/JobQueue.m
@@ -4,6 +4,7 @@
 
 RCT_EXTERN_METHOD(addJob:(NSDictionary *)job)
 RCT_EXTERN_METHOD(removeJob:(NSDictionary *)job)
+RCT_EXTERN_METHOD(updateJob:(NSDictionary *)job)
 RCT_EXTERN_METHOD(removeJobsByWorkerName:(NSString *)workerName)
 RCT_EXTERN_METHOD(getNextJob:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(getJobsForWorker:(NSString *)name count:(NSInteger) count resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
