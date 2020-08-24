82b792277eb43119d9351cef4a0c8f39236a9246
Hanno Gödecke <hgoedecke@cuvent.com>
2020-08-24 16:45:56 +0200

    Improving UI in example

--- example/App.tsx
+++ example/App.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 import queue from '../src/Queue';
 import { Worker } from '../src/Worker';
 
 export interface IAppProps {}
 
-export interface IAppState {}
-let counter = 0;
+export interface IAppState {
+    counter: number;
+}
+
 export default class App extends React.Component<IAppProps, IAppState> {
     constructor(props: IAppProps) {
         super(props);
-        this.state = {};
+        this.state = {
+            counter: 0,
+        };
     }
     componentDidMount() {
         queue.configure({
@@ -23,29 +27,49 @@ export default class App extends React.Component<IAppProps, IAppState> {
             new Worker('testWorker', async (payload) => {
                 return new Promise((resolve) => {
                     setTimeout(() => {
-                        console.log(payload);
+                        this.setState({
+                            counter: payload.id
+                        })
                         resolve();
                     }, 5000);
                 });
             })
         );
     }
+
     public render() {
+        const { counter } = this.state;
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={styles.container}>
                 <Button
                     title='add Job'
                     onPress={() => {
-                        queue.addJob('testWorker', { id: counter++ }, undefined, false);
+                        queue.addJob('testWorker', { id: counter + 1 }, undefined, false);
                     }}
                 />
-                <Button
-                    title='start Queue'
-                    onPress={() => {
-                        queue.start();
-                    }}
-                />
+                <View style={styles.spacing}>
+                    <Button
+                        title='start Queue'
+                        onPress={() => {
+                            queue.start();
+                        }}
+                    />
+                </View>
+                <View style={styles.spacing}>
+                    <Text>Counter: {counter}</Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    spacing: {
+        marginTop: 20,
+    }
+});
