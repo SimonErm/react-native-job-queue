3091f0fd062897c559915e87f945166e5d6c7d9c
Simon Ermler <simon_ermler@web.de>
2023-12-31 01:52:55 +0100

    Use react-native-test-app for example

--- example/App.tsx
+++ example/App.tsx
@@ -1,93 +1,102 @@
 import * as React from 'react';
 import { Button, View } from 'react-native';
 
-import queue from '../src/Queue';
-import { Worker, CANCEL, CancellablePromise } from '../src/Worker';
+import queue, { Worker, CANCEL, CancellablePromise } from 'react-native-job-queue';
 
 export interface IAppProps {}
 
-export interface IAppState {jobId?: string|null}
+export interface IAppState {
+    jobId?: string | null;
+}
 let counter = 0;
 export default class App extends React.Component<IAppProps, IAppState> {
     constructor(props: IAppProps) {
         super(props);
         this.state = {
-          jobId: null,
+            jobId: null,
         };
     }
     componentDidMount() {
         queue.configure({
             onQueueFinish: (executed) => {
                 console.log('FinishedQueue');
-            }
+            },
         });
         queue.addWorker(
-            new Worker('testWorker', (payload) => {
-              let cancel
-                const promise: CancellablePromise<any> = new Promise((resolve, reject) => {
-                    const timeout = setTimeout(() => {
-                        console.log(payload);
-                        resolve();
-                    }, 5000);
+            new Worker(
+                'testWorker',
+                (payload) => {
+                    let cancel;
+                    const promise: CancellablePromise<any> = new Promise((resolve, reject) => {
+                        const timeout = setTimeout(() => {
+                            console.log(payload);
+                            resolve();
+                        }, 5000);
 
-                    cancel = () => {
-                      clearTimeout(timeout)
-                      reject(new Error('canceled'))
-                    }
-                });
+                        cancel = () => {
+                            clearTimeout(timeout);
+                            reject(new Error('canceled'));
+                        };
+                    });
 
-                promise[CANCEL] = cancel
-                return promise
-            },{
-              onStart: ({id}) => this.setState({jobId: id}),
-              onCompletion: () => this.setState({jobId: null}),
-            })
+                    promise[CANCEL] = cancel;
+                    return promise;
+                },
+                {
+                    onStart: ({ id }) => this.setState({ jobId: id }),
+                    onCompletion: () => this.setState({ jobId: null }),
+                }
+            )
         );
     }
     public render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Button
-                    title='add Job'
+                    title="add Job"
                     onPress={() => {
                         queue.addJob('testWorker', { id: counter++ }, undefined, false);
                     }}
                 />
                 <Button
-                    title='cancel Job'
+                    title="cancel Job"
                     onPress={() => {
                         if (this.state.jobId) {
-                            queue.cancelJob(this.state.jobId, { message: 'Canceled' })
+                            queue.cancelJob(this.state.jobId, { message: 'Canceled' });
                         } else {
-                            console.log("no job running");
+                            console.log('no job running');
                         }
                     }}
                 />
                 <Button
-                    title='remove failed Jobs'
+                    title="remove failed Jobs"
                     onPress={async () => {
                         let jobs = await queue.getJobs();
-                        let jobRemovals = jobs.filter(job => job.failed !== '').map(async job => await queue.removeJob(job))
-                        await Promise.all(jobRemovals)
+                        let jobRemovals = jobs
+                            .filter((job) => job.failed !== '')
+                            .map(async (job) => await queue.removeJob(job));
+                        await Promise.all(jobRemovals);
                     }}
                 />
                 <Button
-                    title='requeue failed Jobs'
+                    title="requeue failed Jobs"
                     onPress={async () => {
                         let jobs = await queue.getJobs();
-                        let jobRequeues = jobs.filter(job => job.failed !== '').map(async job => await queue.requeueJob(job))
-                        await Promise.all(jobRequeues)
+                        let jobRequeues = jobs
+                            .filter((job) => job.failed !== '')
+                            .map(async (job) => await queue.requeueJob(job));
+                        await Promise.all(jobRequeues);
                     }}
                 />
                 <Button
-                    title='getJobs'
+                    title="getJobs"
                     onPress={async () => {
                         let jobs = await queue.getJobs();
-                        console.log(jobs)
+                        console.log(jobs);
                     }}
                 />
                 <Button
-                    title='start Queue'
+                    title="start Queue"
                     onPress={() => {
                         queue.start();
                     }}
